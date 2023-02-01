6db9e971f8b0de93c144510e5d55b72e0c5974ee
Kris! <42949874+krstphrrr@users.noreply.github.com>
2023-02-01 10:53:11 -0700

    populate postingest fields procedure

---
+++ pg_functions/postingest_proc_call_populate_fields.sql
@@ -0,0 +1,9 @@
+
+CREATE OR REPLACE PROCEDURE public_test.populate_fields()
+ LANGUAGE sql
+AS $procedure$
+  CALL public_test.postingest_populate_mlra_name('public_test');
+  CALL public_test.postingest_populate_mlrarsym('public_test');
+  CALL public_test.postingest_populate_state('public_test');
+  CALL public_test.postingest_populate_ecoregion('public_test');
+$procedure$
