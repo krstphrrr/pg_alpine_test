3ecb9ffa736e221944f68b5e551f2167aca3d646
Kris! <42949874+krstphrrr@users.noreply.github.com>
2023-02-14 12:18:48 -0700

    populating landcover after tiff ingest

--- pg_functions/postingest_fun_populate_modislandcover.sql
+++ pg_functions/postingest_fun_populate_modislandcover.sql
@@ -1,23 +1,23 @@
-CREATE OR REPLACE FUNCTION public_test.postingest_populate_landcover()
-  RETURNS BOOL
+CREATE OR REPLACE FUNCTION public_test.postingest_populate_landcover(_schema_name text)
+  RETURNS boolean
   LANGUAGE plpgsql
 AS $function$
 DECLARE
   target_schema TEXT;
   field_exists BOOL;
 BEGIN
-  target_schema := 'public_test';
+  target_schema := _schema_name;
 
-  WITH dh AS (
-	  SELECT * FROM public_test."dataHeader" 
+  EXECUTE FORMAT('WITH dh AS (
+	  SELECT * FROM %1$s."dataHeader" 
   ), 
   ras AS (
-    SELECT dh."PrimaryKey", ST_VALUE(rastr.rast,1,dh.wkb_geometry) 
+    SELECT dh."PrimaryKey", public.ST_VALUE(rastr.rast,1,dh.wkb_geometry) 
     FROM gis.modisraster AS rastr
     JOIN dh 
-    ON ST_INTERSECTS(
+    ON public.ST_INTERSECTS(
       dh.wkb_geometry, 
-        ST_CONVEXHULL(rastr.rast)
+        public.ST_CONVEXHULL(rastr.rast)
       )
   ), 
   lg as (
@@ -26,10 +26,12 @@ BEGIN
     JOIN ras 
     ON ras.st_value = leg."Value"
   )
-  UPDATE public_test."geoIndicators" AS ge
+  UPDATE %1$s."geoIndicators" AS ge
   SET modis_landcover = lg."Name"
   FROM lg
   WHERE ge."PrimaryKey" = lg."PrimaryKey";
+  ',target_schema );
+  return true;
 
 END
 $function$
