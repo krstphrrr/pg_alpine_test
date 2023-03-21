2f645033876249ef450b3ed9dffc5e07a77e4d2c
Kris! <42949874+krstphrrr@users.noreply.github.com>
2023-03-21 15:46:19 -0600

    fixed procedure that calls all postingest funs

--- pg_functions/postingest_proc_call_populate_fields.sql
+++ pg_functions/postingest_proc_call_populate_fields.sql
@@ -1,10 +1,12 @@
 
 CREATE OR REPLACE PROCEDURE public_test.populate_fields(_schema_name text)
- LANGUAGE sql
+ LANGUAGE plpgsql
 AS $procedure$
+BEGIN
   PERFORM public_test.postingest_populate_mlra_name(_schema_name);
   PERFORM public_test.postingest_populate_mlrarsym(_schema_name);
   PERFORM public_test.postingest_populate_state(_schema_name);
   PERFORM public_test.postingest_populate_ecoregion(_schema_name);
   PERFORM public_test.postingest_populate_landcover(_schema_name);
+END;
 $procedure$
