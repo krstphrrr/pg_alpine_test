7783d2d9687a0b56ec46f472994af8b4c0691f0b
Kris! <42949874+krstphrrr@users.noreply.github.com>
2023-04-10 16:33:28 -0600

    added date argument to postingest landcover

--- pg_functions/postingest_fun_populate_landcover.sql
+++ pg_functions/postingest_fun_populate_landcover.sql
@@ -1,20 +1,20 @@
-CREATE OR REPLACE FUNCTION public_test.postingest_populate_landcover(_schema_name text)
+CREATE OR REPLACE FUNCTION public_test.postingest_populate_landcover(_schema_name text, _date text)
   RETURNS boolean
   LANGUAGE plpgsql
 AS $function$
 DECLARE
   target_schema TEXT;
   field_exists BOOL;
+  target_date DATE;
 BEGIN
   target_schema := _schema_name;
+  target_date := TO_DATE(_date, 'YYYYMMDD');
 
-  EXECUTE FORMAT('WITH dh AS (
-	  SELECT * FROM %1$s."dataHeader" 
-  ), 
-  ras AS (
+  EXECUTE FORMAT('
+  with ras AS (
     SELECT dh."PrimaryKey", public.ST_VALUE(rastr.rast,1,dh.wkb_geometry) 
     FROM gis.modisraster AS rastr
-    JOIN dh 
+    JOIN (SELECT "PrimaryKey", wkb_geometry from %1$s."dataHeader" where "DateLoadedInDb" between %2$L and now() ) as dh
     ON public.ST_INTERSECTS(
       dh.wkb_geometry, 
         public.ST_CONVEXHULL(rastr.rast)
@@ -22,15 +22,17 @@ BEGIN
   ), 
   lg as (
     SELECT ras."PrimaryKey", leg."Name"
-    FROM gis.modis_classes AS leg
+    FROM public.modis_classes AS leg
     JOIN ras 
     ON ras.st_value = leg."Value"
   )
   UPDATE %1$s."geoIndicators" AS ge
   SET modis_landcover = lg."Name"
   FROM lg
-  WHERE ge."PrimaryKey" = lg."PrimaryKey";
-  ',target_schema );
+  WHERE ge."PrimaryKey" = lg."PrimaryKey"
+				 AND 
+		"DateLoadedInDb" between %2$L and now();
+  ',target_schema, target_date );
   return true;
 
 END
--- pg_functions/postingest_proc_call_populate_fields.sql
+++ pg_functions/postingest_proc_call_populate_fields.sql
@@ -1,12 +1,14 @@
 
-CREATE OR REPLACE PROCEDURE public_test.populate_fields(_schema_name text)
- LANGUAGE plpgsql
+CREATE OR REPLACE PROCEDURE public_test.postingest_populate_fields(
+	IN _schema_name text,
+	IN _date text)
+LANGUAGE 'plpgsql'
 AS $procedure$
 BEGIN
   PERFORM public_test.postingest_populate_mlra_name(_schema_name);
   PERFORM public_test.postingest_populate_mlrarsym(_schema_name);
   PERFORM public_test.postingest_populate_state(_schema_name);
   PERFORM public_test.postingest_populate_ecoregion(_schema_name);
-  PERFORM public_test.postingest_populate_landcover(_schema_name);
+  PERFORM public_test.postingest_populate_landcover(_schema_name, _date);
 END;
 $procedure$
